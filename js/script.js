45864982a50e16da70831e452463d0fa660ad234
ashna-24 <93767921+ashna-24@users.noreply.github.com>
2022-09-28 16:19:47 +0530

    FILES commit

--- js/script.js
+++ js/script.js
@@ -1,207 +1,179 @@
-
-
 function validateform(){
-     var uname=document.forms['myform']['uname'];
-     var gender=document.forms['myform']['gender'];
-     var address=document.forms['myform']['address'];
-     var age=document.forms['myform']['age'];
-     var email=document.forms['myform']['email'];
-     var password=document.forms['myform']['password'];
-     var number=document.forms['myform']['number'];
-     var telephone=document.forms['myform']['telephone'];
-     var qualify=document.forms['myform']['study'];
-     var datetime=document.forms['myform']['datetime'];
-     var proof=document.forms['myform']['proof'];
-     var uploadfile=document.forms['myform']['uploadfile'];
+   var uname=document.forms['myform']['uname'];
+   var gender=document.forms['myform']['gender'];
+   var address=document.forms['myform']['address'];
+   var age=document.forms['myform']['age'];
+   var email=document.forms['myform']['email'];
+   var password=document.forms['myform']['password'];
+   var number=document.forms['myform']['number'];
+   var telephone=document.forms['myform']['telephone'];
+   var qualify=document.forms['myform']['study'];
+   var datetime=document.forms['myform']['datetime'];
+   var uploadfile=document.forms['myform']['uploadfile'];
 
-     nameValidate(uname);
-     genderValidate(gender);
-     addressValidate(address);
-     ageValidate(age);
-     emailValidate(email);
-     passwordValidate(password);
-     numberValidate(number);
-     telephoneValidate(telephone);
-     qualifyValidate(qualify);
-     datetimeValidate(datetime);
-     proofValidate(proof);
-     fileValidate(uploadfile);
-     return false;
+   nameValidate(uname);
+   genderValidate(gender);
+   addressValidate(address);
+   ageValidate(age);
+   emailValidate(email);
+   passwordValidate(password);
+   numberValidate(number);
+   telephoneValidate(telephone);
+   qualifyValidate(qualify);
+   datetimeValidate(datetime);
+   fileValidate(uploadfile);
+   return false;
 }
 
-function errorValidate(){
-    document.getElementById('name_div').style.visibility="visible";
-    document.getElementById('gender_div').style.visibility="visible";
-    document.getElementById('address_div').style.visibility="visible";
-    document.getElementById('age_div').style.visibility="visible";
-    document.getElementById('email_div').style.visibility="visible";
-    document.getElementById('pswd_div').style.visibility="visible";
-    document.getElementById('number_div').style.visibility="visible"
-    document.getElementById('datetime_div').style.visibility="visible";
-    document.getElementById('tel_div').style.visibility="visible";
-    document.getElementById('qualify_div').style.visibility="visible";
-    document.getElementById('proof_div').style.visibility="visible"; 
-    document.getElementById('uploadfile_div').style.visibility="visible";
+function errorValidate(id){
+
+  document.getElementById(id).style.visibility="visible";
+}
+
+function successValidate(id){
+  document.getElementById(id).innerHTML="";
 }
 
 function nameValidate(uname){
-     
-     if(uname.value==""){
-       errorValidate();
-       uname.style.border="1px solid red";  
-       return false;
-     }
-     else if(uname.value.length<3){
-        errorValidate();
-        uname.style.border="1px solid red"; 
-        return false;
-     }
-     else{
-        document.getElementById("name_div").innerHTML="";
-        uname.style.border="1px solid green";
-        return true;
-     }
+   
+   if(uname.value==""){
+     errorValidate('name_div');
+     uname.style.border="1px solid red";  
+     return false;
+   }
+   else if(uname.value.length<3){
+      errorValidate('name_div');
+      uname.style.border="1px solid red"; 
+      return false;
+   }
+   else{
+      successValidate('name_div');
+      uname.style.border="1px solid green";
+      return true;
+   }
 }
 
 function genderValidate(gender){
-     if(gender.value==""){
-       errorValidate();  
-       return false;
-     }else{
-        document.getElementById("gender_div").innerHTML="";
-        return true;
-     }
+   if(gender.value==""){
+     errorValidate('gender_div');  
+     return false;
+   }else{
+      successValidate('gender_div')
+      return true;
+   }
 }
 
 function addressValidate(address){    
-     if(address.value==""){
-       errorValidate();
-       address.style.border="1px solid red";  
-       return false;
-     }
-     else if(address.value.length<8){
-        errorValidate();
-        address.style.border="1px solid red"; 
-        return false;
-     }
-     else{
-        document.getElementById("address_div").innerHTML="";
-        address.style.border="1px solid green";
-        return true;
-     }
+   if(address.value==""){
+     errorValidate('address_div');
+     address.style.border="1px solid red";  
+     return false;
+   }
+   else if(address.value.length<8){
+      errorValidate('address_div');
+      address.style.border="1px solid red"; 
+      return false;
+   }
+   else{
+      successValidate('address_div')
+      address.style.border="1px solid green";
+      return true;
+   }
 }
 
 function ageValidate(age){
-     if(age.value==""){
-        errorValidate();
-       return false;
-     }else{
-        document.getElementById("age_div").innerHTML="";
-        return true;
-     }
+   if(age.value==""){
+      errorValidate('age_div');
+     return false;
+   }else{
+      successValidate('age_div');
+      return true;
+   }
 }
 
 function emailValidate(email){
-     if(email.value==""){
-       errorValidate();
-       email.style.border="1px solid red";  
-       return false;
-     }else{
-        email.style.border="1px solid green";
-        email_div.innerHTML="";
-        return true;
-     }
+   if(email.value==""){
+     errorValidate('email_div');
+     email.style.border="1px solid red";  
+     return false;
+   }else{
+      successValidate('email_div');
+      email.style.border="1px solid green";
+      return true;
+   }
 }
 
 function passwordValidate(password){
-     if(password.value==""){
-      errorValidate(); 
-       password.style.border="1px solid red";  
-       return false;
-     }else{
-        password.style.border="1px solid green";
-        pswd_div.innerHTML="";
-        return true;
-     }
+   if(password.value==""){
+    errorValidate('pswd_div'); 
+     password.style.border="1px solid red";  
+     return false;
+   }else{
+      successValidate('pswd_div');
+      password.style.border="1px solid green";
+      return true;
+   }
 }
 
 function numberValidate(number){
-     if(number.value==""){
-       errorValidate();
-       number.style.border="1px solid red";  
-       return false;
-     }/*
-     else if(number.value.length<=10){
-        errorValidate();
-        number.style.border="1px solid red"; 
-        return false;
-     }*/else{
-        number.style.border="1px solid green";
-        number_div.innerHTML="";
-        return true;
-     }
+   if(number.value==""){
+     errorValidate('number_div');
+     number.style.border="1px solid red";  
+     return false;
+   }
+   else{
+      successValidate('number_div');
+      number.style.border="1px solid green";
+      return true;
+   }
 }
 
 function telephoneValidate(telephone){
-     if(telephone.value==""){
-       errorValidate();
-       telephone.style.border="1px solid red";  
-       return false;
-     }/*
-     else if(telephone.value.length<=10){
-       errorValidate();
-        telephone.style.border="1px solid red"; 
-        return false;
-     }*/else{
-        telephone.style.border="1px solid green";
-        tel_div.innerHTML="";
-        return true;
-     }
+   if(telephone.value==""){
+     errorValidate('tel_div');
+     telephone.style.border="1px solid red";  
+     return false;
+   }
+   else{
+      successValidate('tel_div');
+      telephone.style.border="1px solid green";
+      return true;
+   }
 }
 
 function qualifyValidate(qualify){     
-     if(qualify.value==""){
-       errorValidate();
-       qualify.style.border="1px solid red";  
-       return false;
-     }else{
-        qualify.style.border="1px solid green";
-        qualify_div.innerHTML="";
-        return true;
-     }
+   if(qualify.value==""){
+     errorValidate('qualify_div');
+     qualify.style.border="1px solid red";  
+     return false;
+   }else{
+      successValidate('qualify_div');
+      qualify.style.border="1px solid green";
+      return true;
+   }
 }
 
 function datetimeValidate(datetime){
-     if(datetime.value==""){
-       errorValidate();
-       datetime.style.border="1px solid red";  
-       return false;
-     }else{
-        datetime.style.border="1px solid green";
-        datetime_div.innerHTML="";
-        return true;
-     }
-}
-
-function proofValidate(proof){
-     if(proof.value==""){
-       errorValidate();
-       return false;
-     }else{
-        document.getElementById("proof_div").innerHTML="";
-        return true;
-     }
+   if(datetime.value==""){
+     errorValidate('datetime_div');
+     datetime.style.border="1px solid red";  
+     return false;
+   }else{
+      successValidate('datetime_div');
+      datetime.style.border="1px solid green";
+      return true;
+   }
 }
 
 
 function fileValidate(uploadfile){     
-     if(uploadfile.value==""){
-       errorValidate();
-       uploadfile.style.border="1px solid red";  
-       return false;
-     }else{
-        uploadfile.style.border="1px solid green";
-        uploadfile_div.innerHTML="";
-        return true;
-     }
+   if(uploadfile.value==""){
+     errorValidate('uploadfile_div');
+     uploadfile.style.border="1px solid red";  
+     return false;
+   }else{
+      successValidate('uploadfile_div');
+      uploadfile.style.border="1px solid green";
+      return true;
+   }
 }
